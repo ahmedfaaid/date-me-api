569f330c3c55caa1fc9815e980c81960417e95c2
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2024-12-01 23:27:03 +0000

    order middlewares and use logger

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,13 @@
 import { OpenAPIHono } from '@hono/zod-openapi';
 import notFound from './middlewares/not-found';
 import onError from './middlewares/on-error';
+import { logger } from 'hono/logger';
 
 const app = new OpenAPIHono();
 
 app.notFound(notFound);
-
-app.onError(onError)
+app.onError(onError);
+app.use(logger());
 
 app.get('/', (c) => {
   return c.json({
