2f2a928f3de59a31085395f94f06c5f18d6cb357
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2024-12-01 22:51:21 +0000

    not found middleware

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,14 @@
 import { OpenAPIHono } from '@hono/zod-openapi';
+import { NOT_FOUND as NOT_FOUND_MESSAGE } from './lib/http-status-phrases';
+import { NOT_FOUND } from './lib/http-status-codes';
 
 const app = new OpenAPIHono();
+
+app.notFound((c) => {
+  return c.json({
+    message: `${NOT_FOUND_MESSAGE} - ${c.req.path}`
+  }, NOT_FOUND);
+});
 
 app.get('/', (c) => {
   return c.json({
