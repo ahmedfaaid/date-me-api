4dcc1016c055e3a7bb2e58c4f37eb8edcc3df872
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2024-12-01 22:58:03 +0000

    on error middleware

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import { OpenAPIHono } from '@hono/zod-openapi';
 import { NOT_FOUND as NOT_FOUND_MESSAGE } from './lib/http-status-phrases';
-import { NOT_FOUND } from './lib/http-status-codes';
+import { INTERNAL_SERVER_ERROR, NOT_FOUND, OK } from './lib/http-status-codes';
+import { StatusCode } from 'hono/utils/http-status';
 
 const app = new OpenAPIHono();
 
@@ -9,6 +10,27 @@ app.notFound((c) => {
     message: `${NOT_FOUND_MESSAGE} - ${c.req.path}`
   }, NOT_FOUND);
 });
+
+app.onError((error, c) => {
+  const currentStatus = "status" in error
+    ? error.status
+    : c.newResponse(null).status;
+  const statusCode = currentStatus !== OK
+    ? (currentStatus as StatusCode)
+    : INTERNAL_SERVER_ERROR;
+  // eslint-disable-next-line node/prefer-global/process
+  const env = process.env?.NODE_ENV;
+  return c.json(
+    {
+      message: error.message,
+
+      stack: env === "production"
+        ? undefined
+        : error.stack,
+    },
+    statusCode,
+  );
+})
 
 app.get('/', (c) => {
   return c.json({
