b5494ef527a6f11933c146501ff4e69427f103f3
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2024-12-01 21:46:56 +0000

    initial setup

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import { Hono } from 'hono';
+
+const app = new Hono();
+
+app.get('/', c => {
+  return c.json({
+    message: 'Hello from Date ME'
+  });
+});
+
+export default {
+  port: 5555,
+  fetch: app.fetch
+};
