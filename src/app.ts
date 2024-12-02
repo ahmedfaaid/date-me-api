a380f4477d325eaa6829dec5fcb7e954dc8c1e88
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2024-12-02 09:33:35 +0000

    add app bindings

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,25 @@ import { OpenAPIHono } from '@hono/zod-openapi';
 import notFound from './middlewares/not-found';
 import onError from './middlewares/on-error';
 import pinoLogger from '@/middlewares/pino-logger';
+import { PinoLogger } from 'hono-pino';
 
-const app = new OpenAPIHono();
+type AppBindings = {
+  Variables: {
+    logger: PinoLogger;
+  };
+};
+
+const app = new OpenAPIHono<AppBindings>();
 
 app.notFound(notFound);
 app.onError(onError);
 app.use(pinoLogger());
+
+app.get('/error', (c) => {
+  c.status(422);
+  c.var.logger.info('Some logs here');
+  throw new Error("Man something ain't right");
+});
 
 app.get('/', (c) => {
   return c.json({
