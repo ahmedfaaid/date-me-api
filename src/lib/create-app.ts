76870ff478dacb3d251e0305b3ab710bc6c849bf
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2024-12-02 20:11:20 +0000

    abstract app bindings types

--- src/lib/create-app.ts
+++ src/lib/create-app.ts
@@ -1,14 +1,8 @@
 import notFound from '@/middlewares/not-found';
 import onError from '@/middlewares/on-error';
 import pinoLogger from '@/middlewares/pino-logger';
+import { AppBindings } from '@/types';
 import { OpenAPIHono } from '@hono/zod-openapi';
-import { PinoLogger } from 'hono-pino';
-
-type AppBindings = {
-  Variables: {
-    logger: PinoLogger;
-  };
-};
 
 export default function createApp() {
   const app = new OpenAPIHono<AppBindings>();
