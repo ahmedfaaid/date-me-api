8a5148f6a02cee2dc1ee7b5d7471aceb46d8bd89
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2024-12-03 21:18:55 +0000

    move routes to create app

--- src/app.ts
+++ src/app.ts
@@ -2,16 +2,4 @@ import createApp from '@/lib/create-app';
 
 const app = createApp();
 
-app.get('/error', (c) => {
-  c.status(422);
-  c.var.logger.info('Some logs here');
-  throw new Error("Man something ain't right");
-});
-
-app.get('/', (c) => {
-  return c.json({
-    message: 'Hello from Date ME'
-  });
-});
-
 export default app;
--- src/lib/create-app.ts
+++ src/lib/create-app.ts
@@ -13,5 +13,17 @@ export default function createApp() {
   app.onError(onError);
   app.use(pinoLogger());
 
+  app.get('/error', (c) => {
+    c.status(422);
+    c.var.logger.info('Some logs here');
+    throw new Error("Man something ain't right");
+  });
+  
+  app.get('/', (c) => {
+    return c.json({
+      message: 'Hello from Date ME'
+    });
+  });
+
   return app;
 }
