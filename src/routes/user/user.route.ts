af6e2f91e5876b54cab29593f32dd29152842ec8
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2024-12-12 11:30:56 +0000

    pass select schema to route response

--- src/routes/user/user.route.ts
+++ src/routes/user/user.route.ts
@@ -1,7 +1,7 @@
+import { selectUsersSchema } from '@/db/schema';
 import * as HttpStatusCodes from '@/lib/http-status-codes';
 import jsonContent from '@/lib/json-content';
 import { createRoute, z } from '@hono/zod-openapi';
-import validator from 'validator';
 
 export const users = createRoute({
   tags: ['users'],
@@ -9,12 +9,7 @@ export const users = createRoute({
   path: '/users',
   responses: {
     [HttpStatusCodes.OK]: jsonContent(
-      z.array(z.object({
-        name: z.string(),
-        phone: z.string().refine(validator.isMobilePhone),
-        email: z.string().email(),
-        password: z.string().min(6)
-      })),
+      z.array(selectUsersSchema),
       'List of users'
     )
   }
