dff2e0ca2d38859f314bbd3b7eaa29b2a8db7855
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2024-12-12 21:11:13 +0000

    create user handler

--- src/routes/user/user.handler.ts
+++ src/routes/user/user.handler.ts
@@ -1,9 +1,17 @@
 import db from '@/db';
+import { users as usersSchema } from '@/db/schema';
 import { AppRouteHandler } from '@/types';
-import { UsersRoute } from './user.route';
+import { password } from 'bun';
+import { CreateUserRoute, UsersRoute } from './user.route';
 
 export const users: AppRouteHandler<UsersRoute> = async (c) => {
   const users = await db.query.users.findMany();
   return c.json(users);
 }
 
+export const createUser: AppRouteHandler<CreateUserRoute> = async (c) => {
+  const user = c.req.valid('json');
+  const hashedPassword = await password.hash(user.password);
+  const [insertedUser] = await db.insert(usersSchema).values({ ...user, password: hashedPassword }).returning();
+  return c.json(insertedUser);
+}
