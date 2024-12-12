3e97d967e98d70172dcd21a131a6d2ef9d880a00
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2024-12-12 21:11:03 +0000

    create user route

--- src/routes/user/user.route.ts
+++ src/routes/user/user.route.ts
@@ -1,6 +1,7 @@
-import { selectUsersSchema } from '@/db/schema';
+import { insertUsersSchema, selectUsersSchema } from '@/db/schema';
 import * as HttpStatusCodes from '@/lib/http-status-codes';
 import jsonContent from '@/lib/json-content';
+import jsonContentRequired from '@/lib/json-content-required';
 import { createRoute, z } from '@hono/zod-openapi';
 
 export const users = createRoute({
@@ -15,4 +16,20 @@ export const users = createRoute({
   }
 });
 
+export const createUser = createRoute({
+  tags: ['users'],
+  method: 'post',
+  path: '/users',
+  request: {
+    body: jsonContentRequired(insertUsersSchema, 'The user to create')
+  },
+  responses: {
+    [HttpStatusCodes.CREATED]: jsonContent(
+      selectUsersSchema,
+      'The created user'
+    )
+  }
+});
+
 export type UsersRoute = typeof users;
+export type CreateUserRoute = typeof createUser;
