e76b3cedd43d4220de6d6da6285509b78737bd6f
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2024-12-12 19:14:52 +0000

    required json content helper

---
+++ src/lib/json-content-required.ts
@@ -0,0 +1,11 @@
+import type { ZodSchema } from '@/types';
+import jsonContent from './json-content';
+
+const jsonContentRequired = <T extends ZodSchema>(schema: T, description: string) => {
+  return {
+    ...jsonContent(schema, description),
+    required: true
+  };
+};
+
+export default jsonContentRequired;
