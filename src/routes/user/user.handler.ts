426859bab77953efe00b9cce3182e8de5ef6cda8
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2024-12-13 17:17:07 +0000

    pass http status code for create user

--- src/routes/user/user.handler.ts
+++ src/routes/user/user.handler.ts
@@ -1,5 +1,6 @@
 import db from '@/db';
 import { users as usersSchema } from '@/db/schema';
+import { CREATED } from '@/lib/http-status-codes';
 import { AppRouteHandler } from '@/types';
 import { password } from 'bun';
 import { CreateUserRoute, UsersRoute } from './user.route';
@@ -13,5 +14,5 @@ export const createUser: AppRouteHandler<CreateUserRoute> = async (c) => {
   const user = c.req.valid('json');
   const hashedPassword = await password.hash(user.password);
   const [insertedUser] = await db.insert(usersSchema).values({ ...user, password: hashedPassword }).returning();
-  return c.json(insertedUser);
+  return c.json(insertedUser, CREATED);
 }
