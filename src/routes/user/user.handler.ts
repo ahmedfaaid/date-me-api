3d412745a7b48d12f4faf389a1846407ca4e00de
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2024-12-13 19:39:46 +0000

    get one user handler

--- src/routes/user/user.handler.ts
+++ src/routes/user/user.handler.ts
@@ -1,9 +1,9 @@
 import db from '@/db';
 import { users as usersSchema } from '@/db/schema';
-import { CREATED } from '@/lib/http-status-codes';
+import { CREATED, OK } from '@/lib/http-status-codes';
+import { CreateUserRoute, GetOneUserRoute, UsersRoute } from '@/routes/user/user.route';
 import { AppRouteHandler } from '@/types';
 import { password } from 'bun';
-import { CreateUserRoute, UsersRoute } from './user.route';
 
 export const users: AppRouteHandler<UsersRoute> = async (c) => {
   const users = await db.query.users.findMany();
@@ -15,4 +15,14 @@ export const createUser: AppRouteHandler<CreateUserRoute> = async (c) => {
   const hashedPassword = await password.hash(user.password);
   const [insertedUser] = await db.insert(usersSchema).values({ ...user, password: hashedPassword }).returning();
   return c.json(insertedUser, CREATED);
+}
+
+export const getOneUser: AppRouteHandler<GetOneUserRoute> = async (c) => {
+  const { id } = c.req.valid('param');
+  const user = await db.query.users.findFirst({
+    where(fields, operators) {
+      return operators.eq(fields.id, id)
+    }
+  });
+  return c.json(user, OK)
 }
