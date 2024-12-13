5ace81d3ed8ed3e3514feaf39c8310da73450d98
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2024-12-13 19:39:57 +0000

    add get one user route and handler to router

--- src/routes/user/user.index.ts
+++ src/routes/user/user.index.ts
@@ -4,6 +4,7 @@ import * as routes from '@/routes/user/user.route';
 
 const router = createRouter()
   .openapi(routes.users, handlers.users)
-  .openapi(routes.createUser, handlers.createUser);
+  .openapi(routes.createUser, handlers.createUser)
+  .openapi(routes.getOneUser, handlers.getOneUser);
 
 export default router;
