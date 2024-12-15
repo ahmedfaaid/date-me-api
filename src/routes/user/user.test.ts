90ed01864ec1bff19d81688b2cce43d19ca22308
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2024-12-15 15:18:42 +0000

    setup user test suite and test body validation on users post

--- src/routes/user/user.test.ts
+++ src/routes/user/user.test.ts
@@ -1,14 +1,41 @@
+import { ZOD_ERROR_MESSAGES } from '@/lib/constants';
 import createApp from '@/lib/create-app';
+import env from '@/lib/env';
 import router from '@/routes/user/user.index';
-import { describe, expect, it } from 'bun:test';
+import { afterAll, beforeAll, describe, expect, it } from 'bun:test';
 import { testClient } from 'hono/testing';
+import { execSync } from "node:child_process";
+import fs from "node:fs";
+
+if (env.NODE_ENV !== "test") {
+  throw new Error("NODE_ENV must be 'test'");
+}
 
 const client = testClient(createApp().route('/', router));
 
-describe('Users list', () => {
-  it('responds with an array of users', async () => {
-    const response = await client.users.$get();
-    const json = await response.json();
-    expect(json).toBeArray();
+describe('users routes', () => {
+  beforeAll(async () => {
+    execSync('bun drizzle-kit push');
+  });
+
+  afterAll(async () => {
+    fs.rmSync('test.db', { force: true });
+  });
+
+  it('post /users validates the body when creating a user', async () => {
+    const response = await client.users.$post({
+      // @ts-expect-error
+      json: {
+        name: "Albus",
+        phone: "0244555555",
+        email: "albus@hogwarts.com",
+      }
+    });
+    expect(response.status).toBe(422);
+    if (response.status === 422) {
+      const json = await response.json();
+      expect(json.error.issues[0].path[0]).toBe('password');
+      expect(json.error.issues[0].message).toBe(ZOD_ERROR_MESSAGES.REQUIRED);
+    }
   });
 });
