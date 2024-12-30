eb57659d3d7cb39dd9ea5530eeba941120130a4e
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2024-12-30 17:03:15 +0000

    add auth to app routes

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,12 @@
 import createApp from '@/lib/create-app';
+import auth from '@/routes/auth/auth.index';
 import index from '@/routes/index.route';
 import user from '@/routes/user/user.index';
 import configureOpenApi from './lib/configure-open-api';
 
 const app = createApp();
 
-const routes = [index, user];
+const routes = [index, user, auth];
 
 configureOpenApi(app);
 
