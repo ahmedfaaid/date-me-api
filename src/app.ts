c8214dc61e95d926f49e12ad56c1bdd813c5069b
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2024-12-31 19:37:46 +0000

    pass profile route to app

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,13 @@
 import createApp from '@/lib/create-app';
 import auth from '@/routes/auth/auth.index';
 import index from '@/routes/index.route';
+import profile from '@/routes/profile/profile.index';
 import user from '@/routes/user/user.index';
 import configureOpenApi from './lib/configure-open-api';
 
 const app = createApp();
 
-const routes = [index, user, auth];
+const routes = [index, user, auth, profile];
 
 configureOpenApi(app);
 
