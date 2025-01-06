262c867910ee28a521b232fdf7307a1c37a39ba8
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2025-01-06 13:03:54 +0000

    add create profile route and handler to router

--- src/routes/profile/profile.index.ts
+++ src/routes/profile/profile.index.ts
@@ -2,6 +2,8 @@ import { createRouter } from '@/lib/create-app';
 import * as handlers from '@/routes/profile/profile.handler';
 import * as routes from '@/routes/profile/profile.route';
 
-const router = createRouter().openapi(routes.profile, handlers.profile);
+const router = createRouter()
+  .openapi(routes.profile, handlers.profile)
+  .openapi(routes.createProfile, handlers.createProfile);
 
 export default router;
