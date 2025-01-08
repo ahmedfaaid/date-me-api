a86ac1fb2ac6070327850b3f4f682298f4c7d7d2
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2025-01-08 20:02:45 +0000

    users schema file

---
+++ src/db/schema/users.ts
@@ -0,0 +1,48 @@
+import env from '@/lib/env';
+import { defaultNow } from '@/lib/timestamp';
+import { relations } from 'drizzle-orm';
+import { integer, sqliteTable, text } from 'drizzle-orm/sqlite-core';
+import {
+  createInsertSchema,
+  createSelectSchema,
+  createUpdateSchema
+} from 'drizzle-zod';
+import { profiles } from './profiles';
+
+export const users = sqliteTable('users', {
+  id: integer('id', { mode: 'number' }).primaryKey({ autoIncrement: true }),
+  email: text('email', { mode: 'text' }).notNull(),
+  password: text('password', { mode: 'text' }).notNull(),
+  createdAt: integer('created_at', { mode: 'timestamp' })
+    .default(defaultNow)
+    .notNull(),
+  updatedAt: integer('updated_at', { mode: 'timestamp' })
+    .default(defaultNow)
+    .$onUpdate(() => new Date())
+    .notNull()
+});
+
+export const userRelations = relations(users, ({ one }) => ({
+  profile: one(profiles, {
+    fields: [users.id],
+    references: [profiles.userId]
+  })
+}));
+
+export const selectUsersSchema = createSelectSchema(users).omit({
+  password: env.NODE_ENV === 'production' ? true : undefined
+});
+export const insertUsersSchema = createInsertSchema(users, {
+  email: (schema) => schema.email()
+}).omit({
+  id: true,
+  createdAt: true,
+  updatedAt: true
+});
+export const updateUsersSchema = createUpdateSchema(users).omit({
+  id: true,
+  email: true,
+  createdAt: true,
+  updatedAt: true,
+  password: true
+});
