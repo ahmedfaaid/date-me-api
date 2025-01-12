bdc62e5ad0d206955f41a93f26cf5ab7b57ebab1
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2025-01-12 13:19:18 +0000

    remove all migrations

--- src/db/migrations/0000_petite_iron_fist.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE `users` (
-	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
-	`name` text NOT NULL,
-	`phone` text NOT NULL,
-	`email` text NOT NULL,
-	`password` text NOT NULL,
-	`created_at` integer DEFAULT (cast((julianday('now') - 2440587.5)*86400000 as integer)),
-	`updated_at` integer
-);
