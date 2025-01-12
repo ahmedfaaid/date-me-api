edebf0416f3f1fdd2a5547ab2340be8d450edf08
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2025-01-12 12:03:42 +0000

    images schema

---
+++ src/db/schema/images.ts
@@ -0,0 +1,34 @@
+import { defaultNow } from '@/lib/timestamp';
+import { relations } from 'drizzle-orm';
+import { integer, sqliteTable, text } from 'drizzle-orm/sqlite-core';
+import { createInsertSchema } from 'drizzle-zod';
+import { profiles } from './profiles';
+import { users } from './users';
+
+export const images = sqliteTable('images', {
+  id: integer('id', { mode: 'number' }).primaryKey({ autoIncrement: true }),
+  postedBy: integer('posted_by', { mode: 'number' }).references(() => users.id),
+  filename: text('filename', { mode: 'text' }).notNull(),
+  mimetype: text('mimetype', { mode: 'text' }).notNull(),
+  path: text('path', { mode: 'text' }).notNull(),
+  createdAt: integer('created_at', { mode: 'timestamp' })
+    .default(defaultNow)
+    .notNull(),
+  updatedAt: integer('updated_at', { mode: 'timestamp' })
+    .default(defaultNow)
+    .$onUpdate(() => new Date())
+    .notNull()
+});
+
+export const imageRelations = relations(images, ({ one }) => ({
+  postedBy: one(profiles, {
+    fields: [images.postedBy],
+    references: [profiles.id]
+  })
+}));
+
+export const insertImageSchema = createInsertSchema(images).omit({
+  id: true,
+  createdAt: true,
+  updatedAt: true
+});
