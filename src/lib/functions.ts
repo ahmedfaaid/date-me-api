706cfb3db83165cf622d60680aa410ac1f69902d
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2025-01-14 12:13:18 +0000

    return extension function

--- src/lib/functions.ts
+++ src/lib/functions.ts
@@ -5,3 +5,11 @@ export function removeExtension(filename: string): string {
   }
   return filename.substring(0, lastDotIndex);
 }
+
+export function returnExtension(filename: string): string {
+  const lastDotIndex = filename.lastIndexOf('.');
+  if (lastDotIndex === -1) {
+    return '';
+  }
+  return filename.substring(lastDotIndex + 1);
+}
