b6db528691254931edf0cf033f795e90a4ae86fb
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2025-01-15 12:22:43 +0000

    update path and error message of validating body when creating a profile test

--- src/routes/profile/profile.test.ts
+++ src/routes/profile/profile.test.ts
@@ -55,8 +55,10 @@ describe('profile routes', () => {
     expect(response.status).toBe(422);
     if (response.status === 422) {
       const json = await response.json();
-      expect(json.error.issues[0].path[0]).toBe('userId');
-      expect(json.error.issues[0].message).toBe(ZOD_ERROR_MESSAGES.REQUIRED);
+      expect(json.error.issues[0].path[0]).toBe('profile');
+      expect(json.error.issues[0].message).toBe(
+        ZOD_ERROR_MESSAGES.INVALID_JSON
+      );
     }
   });
 
