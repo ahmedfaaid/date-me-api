70409e05111c3ac534795a732e753938f6bad152
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2025-01-20 20:39:06 +0000

    add delete profile picture route and handler to images router

--- src/routes/image/image.index.ts
+++ src/routes/image/image.index.ts
@@ -2,9 +2,8 @@ import { createRouter } from '@/lib/create-app';
 import * as handlers from '@/routes/image/image.handler';
 import * as routes from '@/routes/image/image.route';
 
-const router = createRouter().openapi(
-  routes.addProfilePicture,
-  handlers.addProfilePicture
-);
+const router = createRouter()
+  .openapi(routes.addProfilePicture, handlers.addProfilePicture)
+  .openapi(routes.deleteProfilePicture, handlers.deleteProfilePicture);
 
 export default router;
