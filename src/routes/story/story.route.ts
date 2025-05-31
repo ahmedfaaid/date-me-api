75c83da2cf14825f9acf91068f90a39869bde96c
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2025-05-31 15:17:46 +0000

    add story route

--- src/routes/story/story.route.ts
+++ src/routes/story/story.route.ts
@@ -1,8 +1,9 @@
-import { selectStoriesSchema } from '@/db/schema/stories';
+import { insertStoriesSchema, selectStoriesSchema } from '@/db/schema/stories';
 import { notFoundSchema } from '@/lib/constants';
 import createErrorSchema from '@/lib/create-error-schema';
 import * as HttpStatusCodes from '@/lib/http-status-codes';
 import jsonContent from '@/lib/json-content';
+import multipartContent from '@/lib/multipart-content';
 import UserIdParamsSchema from '@/lib/userId-params';
 import { createRoute, z } from '@hono/zod-openapi';
 
@@ -29,4 +30,31 @@ export const userStories = createRoute({
   }
 });
 
+export const addStory = createRoute({
+  tags: ['stories'],
+  method: 'post',
+  path: '/stories/{userId}',
+  request: {
+    params: UserIdParamsSchema,
+    body: multipartContent(
+      z.object({
+        story: insertStoriesSchema,
+        duration: z.coerce.number()
+      }),
+      'The story to add'
+    )
+  },
+  responses: {
+    [HttpStatusCodes.CREATED]: jsonContent(
+      selectStoriesSchema,
+      'The story to add'
+    ),
+    [HttpStatusCodes.UNPROCESSABLE_ENTITY]: jsonContent(
+      createErrorSchema(UserIdParamsSchema),
+      'Validation error(s)'
+    )
+  }
+});
+
 export type UserStoriesRoute = typeof userStories;
+export type AddStoryRoute = typeof addStory;
