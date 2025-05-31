e9cd68c2394ce9aeeefd23c7f20e8290649aba57
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2025-05-31 15:50:55 +0000

    add 24 hours ago timestamp

--- src/lib/timestamp.ts
+++ src/lib/timestamp.ts
@@ -1,3 +1,5 @@
 import { sql } from 'drizzle-orm';
 
 export const defaultNow = sql`(cast((julianday('now') - 2440587.5)*86400000 as integer))`;
+
+export const twentyFourHoursAgo = new Date(Date.now() - 24 * 60 * 60 * 1000);
