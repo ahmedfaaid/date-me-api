66ef2c39e9904dbd35dc83e79e05769d7bcbdef4
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2025-05-31 15:18:55 +0000

    add story handler and route to story router

--- src/routes/story/story.index.ts
+++ src/routes/story/story.index.ts
@@ -2,6 +2,8 @@ import { createRouter } from '@/lib/create-app';
 import * as handlers from '@/routes/story/story.handler';
 import * as routes from '@/routes/story/story.route';
 
-const router = createRouter().openapi(routes.userStories, handlers.userStories);
+const router = createRouter()
+  .openapi(routes.userStories, handlers.userStories)
+  .openapi(routes.addStory, handlers.addStory);
 
 export default router;
